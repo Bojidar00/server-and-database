7732adecbd6e3f4c26deaa1bf7df5db2fffda6dd
Bojidar00 <bojidar_rusalinov@abv.bg>
2023-11-13 08:40:03 +0200

    test: Add api tests

---
+++ tests/testData.js
@@ -0,0 +1,19 @@
+export const transaction = {
+  transactionId: "4qZi2Qap241zcEH4",
+  transactionType: "SALE",
+  orderId: "c66oxMaisTwJQXjD",
+  amount: 10,
+  currency: "EUR",
+  description: "Test transaction",
+  accountId: 1,
+};
+
+export const transaction2 = {
+  transactionId: "46Zi3QapS41zcE84",
+  transactionType: "SALE",
+  orderId: "c66oxMaisTwJQXjD",
+  amount: 10,
+  currency: "EUR",
+  description: "Test transaction",
+  accountId: 2,
+};
